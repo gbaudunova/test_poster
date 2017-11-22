ec1b385c5b4a6e938f616441d9bfa6ec0c6d2525
Gulbustan <gbaudunova@gmail.com>
2017-11-22 06:25:43 +0600

    Add some tests

--- main/tests.py
+++ main/tests.py
@@ -4,33 +4,51 @@ from django.core.urlresolvers import reverse
 import requests
 from .views import *
 
-client = Client()
-input_data = {
-    'title': 'title',
-    'url': 'http://grablib.org'
-    }
-
 
 class ViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.input_data = {
+            'title': 'title',
+            'url': 'http://grablib.org',
+            'descroption': 'Autumn'
+        }
+        self.empty_data = {
+            'title': '',
+            'url': '',
+            'description': ''
+
+        }
+        self.small_data = {
+            'title': 't'
+        }
 
     def test_for_connection(self):
         responce = self.client.get('http://127.0.0.1:8000/main/')
         self.assertEqual(responce.status_code, 200)
+
         self.assertTemplateUsed('template.html')
 
+    def test_check_data(self):
+        responce = self.client.post('/main/', self.input_data, follow=True)
+        self.assertTemplateUsed('template.html')
+        self.assertContains(responce, 'Error', 0)
 
-    def test_catch_data(self):
-        responce = self.client.post()
-        self.assertEqual(responce.status_code, 200)
+    def test_empty_data(self):
+        responce = self.client.post('/main/', self.empty_data, follow=True)
+        self.assertTemplateUsed('template.html')
 
+    def test_length_of_data(self):
+        responce = self.client.post('/main/', self.small_data, follow=True)
+        self.assertTemplateUsed('template.html')
 
+    def test_for_fail_connection(self):
+        responce = self.client.post('/main/', self.small_data)
+        responce = self.client.get('http://127.0.0.1:8000/main/')
+        self.assertNotEqual(responce.status_code, 404)
+        self.assertTemplateUsed('template.html')
 
-
-        # request = self.client.post(reverse('catch_data'), data)
-        # request.client = self.client
-        #
-        # responce = MainFormsCreateView.as_view()(request)
-        # self.assertEqual(responce.status_code, 302)
 
 
 

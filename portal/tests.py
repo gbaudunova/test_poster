e58acced3dff83cd706e525f367e41ff56ae7fa1
Gulbustan <gbaudunova@gmail.com>
2017-11-28 23:30:28 +0600

    #1 Add some tests

--- portal/tests.py
+++ portal/tests.py
@@ -1,22 +1,28 @@
 from django.test import TestCase, Client
+from django.urls import reverse
+
 from .forms import *
 from .models import *
-from django.contrib import auth
-from django.contrib.auth.forms import UserCreationForm
+
 
 client = Client()
 
 
 class PortalTest(TestCase):
     def create_portal(self):
-        portal_models = Portal.objects.create(name='name', user="user")
+        portal_models = Portal.objects.create(name='name1', user="user1")
+
         return Portal.objects.create(name='name', user="user")
-        self.assertEqual(portal_models.name, 'name')
-        self.assertEqual(portal_models.user, 'user')
+
+        self.assertEqual(portal_models.name, 'name1')
+
+        self.assertEqual(portal_models.user, 'user1')
 
     def test_creation(self):
         w = self.create_portal()
+
         self.assertTrue(isinstance(w, Portal))
+
         self.assertEqual(w.__str__(), w.name)
 
 
@@ -24,6 +30,7 @@ class User_Form_Test(TestCase):
 
     def test_UserForm_valid(self):
         form = PortalForm(data={'name': "us", 'user': "user"})
+
         self.assertTrue(form.is_valid())
 
 
@@ -36,30 +43,33 @@ class ViewTest(TestCase):
             'password': 'password'
 
         }
-        self.portalNames = {
-            'name1': 'Hacker News',
-            'name2': 'Reddit',
-            'name3': 'Golang News',
-            'name4': 'Habrahabr'
-        }
-        self.portals = {
-            'portal1': 'Hacker News',
-            'portal2': 'Reddit',
-            'portal3': 'Golang News',
-            'portal4': 'Habrahabr'
-        }
         self.validData = {
-            'name': 'name',
-            'user': 'user'
+            'name': 'Reddit',
+            'user': 'user1'
         }
 
-    def test_if_statement(self):
-        portal_form = PortalForm(data=self.validData)
-        self.assertTrue(portal_form.is_valid())
-        create_new_portal = portal_form.save()
-        self.assertEqual(create_new_portal.name, self.validData['name'])
+    def test_for_connection(self):
+        response = self.client.get('https://127.0.0.1:8000/portal/create/')
+        self.assertTemplateUsed('template.html')
+        self.assertRedirects(response, '/main/', status_code=302, target_status_code=200)
+
+    def test_for_receive_data(self):
+        data = {'login': 'login_user', 'password': 'password_user'}
+        r = self.client.post(reverse('portal:create_portal'), data=data, follow=True)
+        self.assertEqual(r.status_code, 200)
+
+    def test_for_delete_portals(self):
+        resp = self.client.get(reverse('portal:delete_portal', args=[id_portal]), follow=True)
 
 
-
+    # def test_for_create_portals(self):
+    #     portal_models = Portal.objects.create(name='Reddit', user="user1")
+    #     portal_form = PortalForm(data=self.validData)
+    #     self.assertTrue(portal_form.is_valid())
+    #     create_new_portal = portal_form.save()
+    #     # self.assertEqual(create_new_portal.name, self.validData['name'])
+    #     response = client.post(reverse('portal:create_portal'), {'name': 'Reddit', 'portals': 'Hacker News'}, follow=True)
+    #     a = portal_models.objects.filter(name=create_new_portal.name)
+    #     self.assertEqual(create_new_portal.name, jkj)
 
 

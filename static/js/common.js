33ba1b01430e6ffd12e2435ed85b15b7731d7e9d
denisoed <denisod93@gmail.com>
2017-11-29 02:47:48 +0300

    Add some changes

--- static/js/common.js
+++ static/js/common.js
@@ -3,11 +3,42 @@ const messages = $('#messages');
 const selected_portals = $('.selected_portals')
 const sel_portal = document.getElementById('select_portal');
 const name_inp = document.getElementById('id_name');
+let list_portal_name = document.getElementById('portal_name');
+let list_portals = document.getElementById('list_portals');
 
-sel_portal.addEventListener('change', function () {
+sel_portal.addEventListener('change', () => {
     name_inp.value = this.value;
 }, false);
 
+list_portals.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.getAttribute("id") == 'portal_name') {
+        let a = target.innerHTML;
+        c = $(selected_portals).children()
+        if (c.length == 0) {
+            $(selected_portals).append(
+                `<input type="text" value="${a}" name="selected_portal">`
+            );
+        } else {
+            // Dev!
+            for (let i = 0; i < c.length; i++) {
+                if ($(c[i]).val() == a) {
+                    $(c[i]).remove();
+                } 
+                if ($(c[1]).val() != a) {
+                    $(selected_portals).append(
+                        `<input type="text" value="${a}" name="selected_portal">`
+                    );
+                }
+            }
+            // End Dev
+        }
+    }
+}, false);
+
+(function set_default_portal() {
+    selected_portal.val(sel_portal[0].value)
+})();
 
 // Hide message after 4 seconds
 (function hideMessage() {
@@ -16,22 +47,10 @@ sel_portal.addEventListener('change', function () {
     }, 4000);
 })();
 
-(function checkSelectedPortals() {
-    let portals = $('.selected_portal')
-    for (let i = 0; i < portals.length; i++) {
-        $('.selected_portals_form').html('<input type="text" name="selected_portals" value="%s">' % portals[i]);
-    }
-})();
 
 $('.the_portal').click(function (event) {
     elem = $(this);
     $(elem).toggleClass('the_portal_disable');
     $(elem).children('span').toggleClass('lamp_disable');
-    // the_portal = $(elem).text()
-    // a = $(selected_portals).find('<input type="text" value="' + $.trim(the_portal) + '" name="selected_portal">');
-    // if () {
-    // $(selected_portals).remove('<input type="text" value="' + $.trim(the_portal) +'" name="selected_portal">');
-    // } else {
-    // $(selected_portals).append('<input type="text" value="' + $.trim(the_portal) +'" name="selected_portal">');
-    // }
 });
+

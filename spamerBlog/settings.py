b2b4922ee4cb5fb5f64d593974419fa36981a18e
Gulbustan <gbaudunova@gmail.com>
2017-12-05 00:06:44 +0600

    create celery app in project

--- spamerBlog/settings.py
+++ spamerBlog/settings.py
@@ -124,3 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# REDIS related settings
+REDIS_HOST = 'redis'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'

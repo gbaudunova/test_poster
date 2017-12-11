d2286884f4b0187ba8e555d5f9d0dd1bd52f2734
denisoed <denisod93@gmail.com>
2017-12-11 16:09:25 +0300

    Add tests for portal tasks and change auth portal function

--- portal/urls.py
+++ portal/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from portal.views import create_portal, delete_portal
-from .tasks import auth_portal
+from .tasks import get_login_page
 
 
 urlpatterns = [
     url(r'create/$', create_portal, name='create_portal'),
     url(r'delete/(?P<id_portal>\d+)/$', delete_portal, name='delete_portal'),
-    url(r'auth_portal/(?P<log_pass>\w+)/(?P<portal>\w+)/$', auth_portal, name='auth_portal')
-
+    url(r'get_login_page/(?P<portal>\w+)$', get_login_page, name='get_login_page')
 ]
